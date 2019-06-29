0dd39ff6ed97768165424c5bfc95d3fbf56055d2
Holow <alexander@herget-net.de>
2019-06-29 18:08:51 +0200

    test revamping

---
+++ src/test/java/de/unikassel/vs/pdDebug/TCPTest.java
@@ -0,0 +1,73 @@
+package de.unikassel.vs.pdDebug;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TCPTest {
+
+    CommType type = CommType.TCP;
+    String address = Publisher.TCP_ADDRESS;
+
+    Publisher pub;
+    Subscriber sub;
+    TestTools util;
+
+    static final int socketSize = 5;
+    static final int testSize = 3;
+    ArrayList<Subscriber> subList = new ArrayList<>();
+    ArrayList<Publisher> pubList = new ArrayList<>();
+
+    @BeforeEach
+    public void before() {
+
+        util = new TestTools();
+        pub = new Publisher();
+        pubList.add(pub);
+        sub = new Subscriber();
+        subList.add(sub);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    public void after() {
+        for (Subscriber sub : subList) {
+            sub.destroy();
+        }
+        for (Publisher pub : pubList) {
+            pub.destroy();
+        }
+        System.out.println();
+    }
+
+    @Test
+    @Order(1)
+    public void sendTest() {
+        pub.bind(type, address);
+        sub.subscribe(type, address);
+
+        util.testMessage(pub, sub, testSize);
+    }
+
+    @Test
+    @Order(2)
+    public void sendSerializedTest() {
+        pub.bind(type, address);
+        sub.subscribe(type, address);
+
+        util.testSerializedMessage(pub, sub, testSize);
+    }
+
+    @Test
+    @Order(3)
+    public void onePubManySubTest() {
+        pubList.clear();
+        subList.clear();
+        util.testOnePubManySub(pubList, subList, socketSize, testSize, type, address);
+    }
+}
--- src/test/java/de/unikassel/vs/pdDebug/TestTools.java
+++ src/test/java/de/unikassel/vs/pdDebug/TestTools.java
@@ -1,5 +1,8 @@
 package de.unikassel.vs.pdDebug;
 
+import junit.framework.Assert;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TestTools {
@@ -34,5 +37,89 @@ public class TestTools {
         }
 
         return sb.toString();
+    }
+
+    public void testOnePubManySub(ArrayList<Publisher> pubList, ArrayList<Subscriber> subList, int socketSize, int testSize, CommType type, String address) {
+
+        Publisher pub = new Publisher();
+        pub.bind(type, address);
+        pubList.add(pub);
+
+
+        for (int i = 0; i < socketSize; i++) {
+            Subscriber sub = new Subscriber(pub.getContext());
+            sub.subscribe(type, address);
+            subList.add(sub);
+        }
+
+        for (int i = 0; i < testSize; i++) {
+            String toTest = TestTools.randomString();
+            pub.sendMessage(toTest);
+
+            int count = 1;
+            for (Subscriber sub : subList) {
+                System.out.print("Sub " + count++ + ": ");
+                String msg = sub.getMessage();
+                Assert.assertTrue(toTest.equals(msg));
+            }
+            System.out.println();
+        }
+    }
+
+    public void testManyPubOneSub(ArrayList<Publisher> pubList, ArrayList<Subscriber> subList, int socketSize, int testSize, CommType type, String address) {
+
+        Subscriber sub = new Subscriber();
+        subList.add(sub);
+
+        for (int i = 0; i < socketSize; i++) {
+            Publisher pub = new Publisher();
+            pub.bind(type, address);
+            pubList.add(pub);
+        }
+
+        sub.subscribe(type, address);
+
+        for (int i = 0; i < testSize; i++) {
+
+            for (Publisher pub : pubList) {
+                String toTest = TestTools.randomString();
+                pub.sendMessage(toTest);
+                String msg = sub.getMessage();
+                Assert.assertTrue(toTest.equals(msg));
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public void testMessage(Publisher pub, Subscriber sub, int testSize) {
+        for (int i = 0; i < testSize; i++) {
+            try {
+                String msg_send = TestTools.randomString();
+                pub.sendMessage(msg_send);
+                Thread.sleep(300);
+                String msg_received = sub.getMessage();
+
+                Assert.assertTrue(msg_send.equals(msg_received));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void testSerializedMessage(Publisher pub, Subscriber sub, int testSize) {
+        for (int i = 0; i < testSize; i++) {
+            try {
+                String msg_send = TestTools.randomString();
+                pub.sendSerializedMessage(msg_send);
+                Thread.sleep(300);
+                String msg_received = sub.getSerializedMessage();
+
+                Assert.assertTrue(msg_send.equals(msg_received));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
