b1fd9345220c40e3a7c8d4378724d073d1d604ce
xlink9x <44588304+xlink9x@users.noreply.github.com>
2019-07-02 19:56:04 +0200

    added capnzerowrapper repo details

--- src/main/java/de/unikassel/vs/pdDebug/capnzero/Capnzero.java
+++ src/main/java/de/unikassel/vs/pdDebug/capnzero/Capnzero.java
@@ -11,6 +11,7 @@ public class Capnzero {
     static {
         // only works if LD_LIBRARY_PATH includes /path/to/ros-ws/devel/lib and you have
         // built the catkin-workspace with capnzerowrapper included.
+        // https://github.com/xlink9x/capnzerowrapper/
         Native.register("capnzerowrapper");
     }
 }
