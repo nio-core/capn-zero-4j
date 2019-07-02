1ff59e419f15f117fc773cd83d390a2f7e57ceff
link9 <xlink9x@web.de>
2019-07-02 19:52:24 +0200

    receive serialized message with capnzerowrapper

--- src/main/java/de/unikassel/vs/pdDebug/capnzero/Capnzero.java
+++ src/main/java/de/unikassel/vs/pdDebug/capnzero/Capnzero.java
@@ -1,19 +1,16 @@
 package de.unikassel.vs.pdDebug.capnzero;
 
-import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
-import com.sun.jna.Structure;
-import de.unikassel.vs.pdDebug.CommType;
-import de.unikassel.vs.pdDebug.Publisher;
 
 public class Capnzero {
 
     public static native int sendMessage(Pointer socket, int commType, String topic, String message);
+    public static native String receiveSerializedMessage(Pointer socket, int commType);
 
     static {
-        // only works if LD_LIBRARY_PATH includes /path/to/ros-ws/devel/lib
-        // and libcapnzero.so is the updated shared object with the sendMessage-wrapper method
-        Native.register("capnzero");
+        // only works if LD_LIBRARY_PATH includes /path/to/ros-ws/devel/lib and you have
+        // built the catkin-workspace with capnzerowrapper included.
+        Native.register("capnzerowrapper");
     }
 }
